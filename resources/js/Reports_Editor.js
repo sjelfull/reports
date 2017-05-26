7a4cd3d723eb4dcf6323711b5113362ec316af9d
Fred Carlsen <fred@sjelfull.no>
2017-05-26 13:10:20 +0200

    Add code editor to twig code field

---
+++ resources/js/Reports_Editor.js
@@ -0,0 +1,14 @@
+
+var $editor = document.querySelector('.js-reportsEditor');
+
+if ($editor) {
+    CodeMirror.defineMode("htmltwig", function(config, parserConfig) {
+        return CodeMirror.overlayMode(CodeMirror.getMode(config, parserConfig.backdrop || "text/html"), CodeMirror.getMode(config, "twig"));
+    });
+
+    var cmInstance = CodeMirror.fromTextArea($editor, {
+        mode: 'htmltwig',
+        lineNumbers: true,
+        indentUnit: 4,
+    });
+}
