dd7bc0982caf13cd6c9a16f5ea87e4e54745965e
Fred Carlsen <fred@sjelfull.no>
2017-06-19 16:54:00 +0200

    Cast Twig_Markup objects to string

--- variables/ReportsVariable.php
+++ variables/ReportsVariable.php
@@ -15,15 +15,52 @@ namespace Craft;
 
 class ReportsVariable
 {
-    public function prepare ($options = [])
+    public function prepare ($options = [ ])
     {
-        $data = [
-            'columns' => !empty($options['columns']) ? $options['columns'] : [],
-            'rows'    => !empty($options['rows']) ? $options['rows'] : [],
+        $defaultOptions = [
+            'type'    => 'table',
+            'columns' => [ ],
+            'rows'    => [ ],
+            'labels'  => [ ],
+            'data'    => [ ],
         ];
+        $data           = array_merge($defaultOptions, $options);
+        $data['rows'] = $this->safelyConvert($data['rows']);
 
         $json = json_encode($data);
 
         return TemplateHelper::getRaw('%report%' . $json . '%endreport%');
+    }
+
+    public function db ()
+    {
+        return craft()->db->createCommand();
+    }
+
+    private function safelyConvert ($rows = [ ])
+    {
+        $safeTypes = [ 'integer', 'double', 'string', 'NULL' ];
+
+        $newRows = [ ];
+
+        foreach ($rows as $row) {
+            $newRow = [ ];
+
+            foreach ($row as $cell) {
+                if ( !in_array(gettype($cell), $safeTypes) ) {
+                    if ($cell instanceof \Twig_Markup) {
+                        $newRow[] = (string) $cell;
+                    } else {
+                        $newRow[] = $cell;
+                    }
+                } else {
+                    $newRow[] = $cell;
+                }
+            }
+
+            $newRows[] = $newRow;
+        }
+
+        return $newRows;
     }
 }
